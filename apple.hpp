e4e5a63ecba2c0cd1ea86559e55305c8851f28f5
Mihail Croitor <mcroitor@gmail.com>
2024-01-01 18:13:09 +0200

    operators

--- apple.hpp
+++ apple.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include "point.hpp"
 
 class Apple {
@@ -7,5 +8,12 @@ class Apple {
 public:
    Apple();
    Apple(const Point& position);
+   Apple(const Apple& other);
    Point GetPosition() const;
+
+   Apple operator = (const Apple& other);
+   bool operator == (const Apple& other) const;
 };
+
+std::istream& operator >> (std::istream& in, Apple& apple);
+std::ostream& operator << (std::ostream& out, const Apple& apple);
--- game_engine.cpp
+++ game_engine.cpp
@@ -1,5 +1,14 @@
 #include "game_engine.hpp"
+#include "painter.hpp"
 
-GameEngine::GameEngine() {}
-void GameEngine::Init() {}
+GameEngine::GameEngine() {
+    _board = Board(10, 10);
+}
+void GameEngine::Init() {
+    std::vector<std::string> board(
+        _board.GetWidth(),
+        std::string(_board.GetHeight(), '+'));
+    Painter::DrawImage(Point(0, 0), board);
+    //std::cout << _board << std::endl;
+}
 void GameEngine::Run() {}
