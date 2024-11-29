04c5c6a29ed2c8062146488ba8ad6bd021f9c6f3
Mihail Croitor <mihail.croitor@usm.md>
2024-11-29 20:51:22 +0200

    some docs added

--- src/direction.hpp
+++ src/direction.hpp
@@ -2,8 +2,13 @@
 
 #include <iostream>
 
+/// @brief The direction in which the snake moves.
 enum class Direction {
    Top, Left, Right, Bottom
 };
 
+/// @brief Write the direction to the output stream.
+/// @param out 
+/// @param direction 
+/// @return 
 std::ostream& operator<<(std::ostream& out, const Direction& direction);
--- src/point.hpp
+++ src/point.hpp
@@ -3,17 +3,42 @@
 #include <iostream>
 
 struct Point{
+   /// @brief The x coordinate of the point.
    int x;
+   /// @brief The y coordinate of the point.
    int y;
 
+   /// @brief Construct a new Point object
+   /// @param _x 
+   /// @param _y 
    Point(int _x = 0, int _y = 0);
+   /// @brief Copy constructor
+   /// @param other 
    Point(const Point& other);
+   /// @brief Move constructor
    Point(Point&& other) noexcept;
 
+   /// @brief Copy assignment operator
+   /// @param other
+   /// @return Point
    Point operator = (const Point& other);
+   /// @brief Move assignment operator
+   /// @param other 
+   /// @return Point
    Point operator = (Point&& other) noexcept;
+   /// @brief Compare two points
+   /// @param other 
+   /// @return 
    bool operator == (const Point& other) const;
 };
 
+/// @brief Read a point from the input stream.
+/// @param in 
+/// @param point 
+/// @return 
 std::istream& operator >> (std::istream& in, Point& point);
+/// @brief Write a point to the output stream.
+/// @param out 
+/// @param point 
+/// @return 
 std::ostream& operator << (std::ostream& out, const Point& point);
